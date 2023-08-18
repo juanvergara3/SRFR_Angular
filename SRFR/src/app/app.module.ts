4ba43a5a5189bba520d86bbe878f928703c2dca2
Juan Vergara <juan.e.vergara@hotmail.com>
2023-08-18 15:01:59 -0500

    Primeros componentes
    - se elimina el código placeholder de app.component (html y css).
    - se crea el primer componente (main-menu) y un subcomponente (branding-container).
    - se reemplaza favicon.ico por el logo de RMA.
    - cambios menores a la hoja de estilos globales (importación).

--- SRFR/src/app/app.module.ts
+++ SRFR/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { BrandingContainerComponent } from './main-menu/branding-container/branding-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    BrandingContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ SRFR/src/app/main-menu/branding-container/branding-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'branding-container',
+  templateUrl: './branding-container.component.html',
+  styleUrls: ['./branding-container.component.css']
+})
+export class BrandingContainerComponent {
+
+}
