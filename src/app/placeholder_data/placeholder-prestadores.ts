61e25b0ee8535e02e2e11b80605f64e2c2609cbd
Juan Vergara <juan.e.vergara@hotmail.com>
2023-11-14 20:06:16 -0500

    Eliminación de datos de placeholder

--- src/app/placeholder_data/placeholder-prestadores.ts
+++
@@ -1,8 +0,0 @@
-import { Prestador } from "../interfaces/prestador";
-
-export const prestadoresPlaceholderArray: Prestador[] = [
-    { id:1, nombre:'RMA Computadores S.A.S.' },
-    { id:2, nombre:'Jhonatan Rentería' },
-    { id:3, nombre:'Jhon Jairo Hincapie' },
-    { id:4, nombre:'Sergio Galeano' },
-];
